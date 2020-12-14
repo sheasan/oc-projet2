1bb4d1303b5db554562155aef5848ae3a69eb4ac
ali <ali.hassane@outlook.com>
2020-12-14 23:10:50 +0100

    Code pour extraction de liens des livres sur une page

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+from urllib.parse import urljoin
+
+import csv
+
+url = "https://books.toscrape.com/catalogue/page-1.html"
+
+#Récupération d'une page web
+response = requests.get(url)
+
+#Si l'url est valide, execute le bloc d'instructions suivants
+if response.ok:
+
+    #Création d'un objet beautifulsoup
+    soup = BeautifulSoup(response.text, "lxml")
+
+    subtitles = soup.find_all("h3")
+
+    for subtitle in subtitles:
+        partial_books_links = subtitle.a.get("href")
+        complete_books_links = urljoin("https://books.toscrape.com/catalogue/", partial_books_links)
+        print(complete_books_links)
