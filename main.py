6ab61217fc93dc9e6d4962028d66760c1fa1dffd
ali <ali.hassane@outlook.com>
2020-12-20 17:18:02 +0100

    Ajout code écriture fichier csv et récupération image

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import urllib.request
 import csv
 import books
 import math
-
+import os
 
 '''with open("scrapfile.csv", mode="w", newline="") as file:
     fieldnames = ["product_page_url",
@@ -68,6 +68,12 @@ if response.ok:
     soup = BeautifulSoup(response.text, "lxml")
     # Identification et extraction des catégories
     categorys = soup.find(class_="nav nav-list").find_all("a")
+
+    # Création d'un répertoire pour les fichiers csv
+    os.mkdir("/home/ali/Documents/Openclassrooms/Projets/oc-projet2/data")
+    os.mkdir("/home/ali/Documents/Openclassrooms/Projets/oc-projet2/data/csv_files")
+    os.mkdir("/home/ali/Documents/Openclassrooms/Projets/oc-projet2/data/pictures")
+
     for category in categorys[1:]:
         relative_link = category.get("href")
         absolute_link = urljoin("https://books.toscrape.com", relative_link)
@@ -76,37 +82,84 @@ if response.ok:
     # Pour chaque catégorie déterminer la pagination
     for category_element in category_list:
         parse_category_page = parse_page(category_element)
+        category_name = parse_category_page.h1.text
 
-        # Obtenir le nombre de pages par catégorie
-        parse_books_quantity = parse_category_page.select(".form-horizontal > strong:nth-child(2)")[0].text
-        books_quantity_result = int(parse_books_quantity)
-        books_quantity_per_page = 20
-        pages_number = math.ceil(books_quantity_result/books_quantity_per_page)
+        # Création un dossier image par category
+        os.mkdir("/home/ali/Documents/Openclassrooms/Projets/oc-projet2/data/pictures/"+category_name)
 
-        # Si il y'a plus d'une page dans la catégorie
-        if pages_number > 1:
-            for i in range(1, pages_number+1):
-                page_url = urljoin(category_element, f"page-{i}.html")
-                multiple_pages = parse_page(page_url)
+        # Création d'un fichier csv par catégorie
+        with open(("/home/ali/Documents/Openclassrooms/Projets/oc-projet2/data/csv_files/"+category_name+".csv"), mode="w", newline="") as file:
+            fieldnames = ["product_page_url",
+                  "universal_ product_code (upc)",
+                  "title",
+                  "price_including_tax", 
+                  "price_excluding_tax",
+                  "number_available",
+                  "product_description",
+                  "category",
+                  "review_rating",
+                  "image_url"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            # Obtenir le nombre de pages par catégorie
+            parse_books_quantity = parse_category_page.select(".form-horizontal > strong:nth-child(2)")[0].text
+            books_quantity_result = int(parse_books_quantity)
+            books_quantity_per_page = 20
+            pages_number = math.ceil(books_quantity_result/books_quantity_per_page)
 
-                #soup = BeautifulSoup(response.text, "lxml")
-                many_page_subtitles = multiple_pages.find_all("h3")
+            # Si il y'a plus d'une page dans la catégorie
+            if pages_number > 1:
+                for i in range(1, pages_number+1):
+                    page_url = urljoin(category_element, f"page-{i}.html")
+                    multiple_pages = parse_page(page_url)
 
-                for many_page_subtitle in many_page_subtitles:
-                    partial_books_links = many_page_subtitle.a.get("href").replace("../../..", "catalogue")
+                    #soup = BeautifulSoup(response.text, "lxml")
+                    many_page_subtitles = multiple_pages.find_all("h3")
+
+                    for many_page_subtitle in many_page_subtitles:
+                        partial_books_links = many_page_subtitle.a.get("href").replace("../../..", "catalogue")
+                        complete_books_links = urljoin("https://books.toscrape.com", partial_books_links)
+                        print("L'url du livre est :", complete_books_links)
+                        all_data_books = books.scrap_book(complete_books_links)
+
+                        writer.writerow({"product_page_url": complete_books_links,
+                                 "universal_ product_code (upc)": books.scrap_book.book_data["universal_ product_code (upc)"],
+                                 "title": books.scrap_book.book_data["title"],
+                                 "price_including_tax": books.scrap_book.book_data["price_including_tax"],
+                                 "price_excluding_tax": books.scrap_book.book_data["price_excluding_tax"],
+                                 "number_available": books.scrap_book.book_data["number_available"],
+                                 "product_description": books.scrap_book.book_data["product_description"],
+                                 "category": books.scrap_book.book_data["category"],
+                                 "review_rating": books.scrap_book.book_data["review_rating"],
+                                 "image_url": books.scrap_book.book_data["image_url"]})
+                        
+                        # Telechargement de l'image de la couverture du livre
+                        urllib.request.urlretrieve(books.scrap_book.book_data["image_url"], "/home/ali/Documents/Openclassrooms/Projets/oc-projet2/data/pictures/"+category_name+"/"+books.scrap_book.book_data["title"])
+
+            # Si présence d'une seule page dans la catégorie
+            else:
+                single_page_subtitles = parse_category_page.find_all("h3")
+
+                for single_page_subtitle in single_page_subtitles:
+                    partial_books_links = single_page_subtitle.a.get("href").replace("../../..", "catalogue")
                     complete_books_links = urljoin("https://books.toscrape.com", partial_books_links)
-                    print("L'url du livre est :", complete_books_links)
+                    print(complete_books_links)
                     all_data_books = books.scrap_book(complete_books_links)
 
-        # Si présence d'une seule page dans la catégorie
-        else:
-            single_page_subtitles = parse_category_page.find_all("h3")
+                    # Telechargement de l'image de la couverture du livre
+                    urllib.request.urlretrieve(books.scrap_book.book_data["image_url"], "/home/ali/Documents/Openclassrooms/Projets/oc-projet2/data/pictures/"+category_name+"/"+books.scrap_book.book_data["title"])
 
-            for single_page_subtitle in single_page_subtitles:
-                partial_books_links = single_page_subtitle.a.get("href").replace("../../..", "catalogue")
-                complete_books_links = urljoin("https://books.toscrape.com", partial_books_links)
-                print(complete_books_links)
-                all_data_books = books.scrap_book(complete_books_links)
+                    writer.writerow({"product_page_url": complete_books_links,
+                                 "universal_ product_code (upc)": books.scrap_book.book_data["universal_ product_code (upc)"],
+                                 "title": books.scrap_book.book_data["title"],
+                                 "price_including_tax": books.scrap_book.book_data["price_including_tax"],
+                                 "price_excluding_tax": books.scrap_book.book_data["price_excluding_tax"],
+                                 "number_available": books.scrap_book.book_data["number_available"],
+                                 "product_description": books.scrap_book.book_data["product_description"],
+                                 "category": books.scrap_book.book_data["category"],
+                                 "review_rating": books.scrap_book.book_data["review_rating"],
+                                 "image_url": books.scrap_book.book_data["image_url"]})
+    
             
 
 
