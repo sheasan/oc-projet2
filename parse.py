a731f0f4626d81d094db8cac610e0b5f1caf0c6c
ali <ali.hassane@outlook.com>
2021-01-11 21:34:34 +0100

    Ajout fichier séparation module avec fonction de parsing

---
+++ parse.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def parse_page(url):
+    """La fonction parse_page() effectue le parsing d'un site internet à partir de son lien url"""
+    response = requests.get(url)
+    if response.ok:
+        parse_page = BeautifulSoup(response.content, "lxml")
+    else:
+        print("Vérifier l'url")
+    return parse_page
