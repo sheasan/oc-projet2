b08be7056a5dee4206db66b3790b4895e1152b11
ali <ali.hassane@outlook.com>
2021-01-20 18:58:15 +0100

    Suppresion code superflu

--- book.py
+++ book.py
@@ -1,4 +1,3 @@
-import requests
 from urllib.parse import urljoin
 import argparse
 import parse
@@ -9,58 +8,51 @@ def scrap_book(url):
     Extrait les différentes données d'un livre à partir d'un lien url d'un livre du site https://books.toscrape.com/
     """
 
-    # Récupération d'une page web
-    response = requests.get(url)
+    # Utilisation de la fonction de parsing depuis un module tier
+    soup = parse.parse_page(url)
 
-    # Si l'url est valide, execute le bloc d'instructions suivants
-    if response.ok:
+    # Récupération du titre du livre
+    title = soup.h1.string
 
-        # Utilisation de la fonction de parsing depuis un module tier
-        soup = parse.parse_page(url)
+    # Récupération de la catégorie
+    category = soup.select(".breadcrumb > li:nth-child(3) > a")[0].text
 
-        # Récupération du titre du livre
-        title = soup.h1.string
+    # Récupération de la notation avec nombre d'étoiles
+    star = soup.find(class_="star-rating")
+    notation = star["class"][1]
 
-        # Récupération de la catégorie
-        category = soup.select(".breadcrumb > li:nth-child(3) > a")[0].text
+    # Récupération de la description du livre
+    description = soup.find("meta", attrs={"name": "description"}).attrs["content"]
 
-        # Récupération de la notation avec nombre d'étoiles
-        star = soup.find(class_="star-rating")
-        notation = star["class"][1]
+    # Récuperation du lien de l'image de la couverture du livre
+    image_url_relative = soup.img.attrs["src"]
+    image_url = urljoin("https://books.toscrape.com", image_url_relative)
 
-        # Récupération de la description du livre
-        description = soup.find("meta", attrs={"name": "description"}).attrs["content"]
+    # Récupération de différentes données requises du livre
+    elements = soup.find_all("td")
 
-        # Récuperation du lien de l'image de la couverture du livre
-        image_url_relative = soup.img.attrs["src"]
-        image_url = urljoin("https://books.toscrape.com", image_url_relative)
+    # Extraction du number available et nettoyage pour obtenir que le nombre
+    number_available_string = elements[5].text
+    number_available_string_list = number_available_string.split()
+    number_string = number_available_string_list[2].replace("(", "")
+    number = int(number_string)
 
-        # Récupération de différentes données requises du livre
-        elements = soup.find_all("td")
+    book_data = {
+        "product_page_url" : url,
+        "universal_ product_code (upc)": elements[0].text,
+        "title": title,
+        "price_including_tax": elements[2].text,
+        "price_excluding_tax": elements[3].text,
+        "number_available": number,
+        "product_description": description,
+        "category": category,
+        "review_rating": notation,
+        "image_url": image_url
+    }
 
-        # Extraction du number available et nettoyage pour obtenir que le nombre
-        number_available_string = elements[5].text
-        number_available_string_list = number_available_string.split()
-        number_string = number_available_string_list[2].replace("(", "")
-        number = int(number_string)
+    # Affichage de la catégorie
+    print(book_data["category"])
 
-        book_data = {
-            "product_page_url" : url,
-            "universal_ product_code (upc)": elements[0].text,
-            "title": title,
-            "price_including_tax": elements[2].text,
-            "price_excluding_tax": elements[3].text,
-            "number_available": number,
-            "product_description": description,
-            "category": category,
-            "review_rating": notation,
-            "image_url": image_url
-        }
-
-        # Affichage de la catégorie
-        print(book_data["category"])
-    else:
-        print("Vérifier l'url")
     return book_data
 
 
